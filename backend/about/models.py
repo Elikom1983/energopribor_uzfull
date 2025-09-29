fde81d1789302b39d5d5732a819db514f0557e74
Elikom1983 <65336695+Elikom1983@users.noreply.github.com>
2025-09-29 16:14:38 +0500

    Xabar

--- backend/about/models.py
+++ backend/about/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from parler.models import TranslatableModel, TranslatedFields
+from django.utils.html import format_html
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+from django.conf import settings  
+
+
 
 # Create your models here.
